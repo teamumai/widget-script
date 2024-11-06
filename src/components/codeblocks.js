395fcb1ac7c4af83797b5ecce30cd98c46d5a288
Murtaza <mudza100@gmail.com>
2024-11-06 13:52:37 +0800

    updated button id parsing to support html id

--- src/components/codeblocks.js
+++ src/components/codeblocks.js
@@ -1,9 +1,13 @@
 export default function widgetCode({ ids = "", apiKey, widgetType = "reservation" }) {
   const domId =
     ids
-      ?.split(",")
-      ?.filter((i) => i?.trim?.()?.length)
-      ?.map((id) => `"#${id.trim()}"`) || "";
+      .split(",")
+      .filter((i) => i.trim().length)
+      .map(
+        (id) =>
+          // Trim whitespace and remove invalid characters for HTML ID compatibility
+          `"${id.trim().replace(/[^a-zA-Z0-9-_:.]/g, "")}"`
+      ) || "";
 
   return `<script src="https://widget.letsumai.com/dist/embed.min.js" data-api-key="${apiKey}" data-widget-type="${widgetType}"></script>
 <script type="text/javascript">
